605df413b13ed34bef7abc28eeb13873b5f2438b
Kam Lasater <kam@cyclic.sh>
2021-10-14 22:42:49 -0400

    Init of docusaurus

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/ckl/git/cyclic/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/ckl/git/cyclic/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/ckl/git/cyclic/docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/ckl/git/cyclic/docs/src/css/custom.css"),
+];
